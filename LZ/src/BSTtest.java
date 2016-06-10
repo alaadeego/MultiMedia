202026119d996ee0f7d13e15a8217813b5d1e57f
alaadeego <alaamimi@hotmail.com>
2016-06-10 14:57:46 +0200

    Add files via upload

---
+++ LZ/src/BSTtest.java
@@ -0,0 +1,18 @@
+//import org.junit.Test;
+public class BSTtest {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BST tree=new BST();
+		tree.add(30);
+		tree.add(50);
+		tree.add(90);
+		tree.add(25);
+		tree.add(40);
+		tree.traverse();
+
+		
+	}
+
+}
---
+++ LZ/src/Node.java
@@ -0,0 +1,41 @@
+
+ class Node {
+  Node left;
+  Node right;
+  Node parent ;
+  String code;
+  int frequency; 
+  char symbol;
+
+Node(int v, char c)
+{
+	this.left=null;
+	this.right=null;
+	this.parent=null;
+	this.code=null;
+	this.frequency=v;
+	this.symbol=c;
+	
+	
+}
+Node(int v)
+{
+	this.left=null;
+	this.right=null;
+	this.parent=null;
+	this.code=null;
+	this.frequency=v;
+	this.symbol=' ';	
+}
+Node()
+{
+	this.left=null;
+	this.right=null;
+	this.parent=null;
+	this.code=null;
+	this.frequency=0;
+	this.symbol=' ';	
+}
+
+
+}
